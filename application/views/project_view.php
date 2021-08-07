a7e4f521e95409ab68048ec2c0220c63c7a43ad6
didiroyadi123 <didiroyadi123@gmail.com>
2021-08-07 23:47:02 +0700

    commit

---
+++ application/views/project_view.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Menu Project</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900&display=swap" rel="stylesheet">
+
+        
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/open-iconic-bootstrap.min.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/animate.css'?>">
+    
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/owl.carousel.min.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/owl.theme.default.min.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/magnific-popup.css'?>">
+
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/aos.css'?>">
+
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/ionicons.min.css'?>">
+    
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/flaticon.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/icomoon.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/style.css'?>">
+  </head>
+  <body>
+  <?php $this->load->view('header') ;?>
+    
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('<?php echo base_url().'assets/images/50.jpg'?>');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Project</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Project <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section">
+			<div class="container">
+				<div class="row">
+    			<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/11.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Tahura Djuanda Bandung</a></h3>
+	    				</div>
+    				</div>
+  				</div>
+  				<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/13.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Curug Cimahi Bandung</a></h3>
+	    				</div>
+    				</div>
+  				</div>
+
+    			<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/14.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Lembah Cilengkrang Kuningan</a></h3>
+	    				</div>
+    				</div>
+    			</div>
+					<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/15.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Talaga Pancar Majalengka</a></h3>
+	    				</div>
+    				</div>
+  				</div>
+  				<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/16.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Buper Trijaya Kuningan</a></h3>
+	    				</div>
+    				</div>
+  				</div>
+  				<div class="col-md-4">
+    				<div class="project mb-4 img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(<?php echo base_url().'assets/images/17.jpg'?>);">
+    					<div class="overlay"></div>
+    					<a href="#" class="btn-site d-flex align-items-center justify-content-center"><span class="icon-subdirectory_arrow_right"></span></a>
+	    				<div class="text text-center p-4">
+	    					<h3><a href="#">Candi Prambahan Yogyakarta</a></h3>
+	    				</div>
+    				</div>
+  				</div>
+    		</div>
+    		<div class="row mt-5">
+          <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+			</div>
+        </section>
+        
+        <?php $this->load->view('footer') ;?>
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+
+  <script src="<?php echo base_url().'assets/js/jquery.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery-migrate-3.0.1.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/popper.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/bootstrap.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery.easing.1.3.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery.waypoints.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery.stellar.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/owl.carousel.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery.magnific-popup.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/aos.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/jquery.animateNumber.min.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/scrollax.min.js'?>"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="<?php echo base_url().'assets/js/google-map.js'?>"></script>
+  <script src="<?php echo base_url().'assets/js/main.js'?>"></script>
+    
+  </body>
+</html>
